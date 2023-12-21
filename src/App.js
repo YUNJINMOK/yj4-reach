b927714e4d5b8147fea2b46cd53bef30fd236134
YUNJINMOK <aprnaprn114@naver.com>
2023-12-21 13:04:38 +0900

    첫번쨰

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
+const name = "리액트"
 function App() {
+  const name = "리액트";
+ 
+  {/*주석은 이렇게 작성합니다*/}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="navbar">
+        <div id="navbar_logo">카카오톡</div>
+        <div id="navbar_meun">
+          <ul>
+            <li><a href="#">카톡 안녕가이드</a></li>
+            <li><a href="#">카톡 설명서</a></li>
+            <li><a href="#">다운로드</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="main">
+        <div id="main_text">
+        <h1>카카오 쇼핑</h1>
+        <p>카톡에서 이뤄지는 오늘의 쇼핑 4번쨰 탬 '카카오 쇼핑'에서<br></br> 제공하는 제품을 빠르고 간편하게 살펴보세요</p>
+        </div>
+      </div>
     </div>
-  );
-}
+   
+  )
 
+    
+
+  
+    
+  
+  
+}
+  
 export default App;
+
+  
+  
+
+    
+    
